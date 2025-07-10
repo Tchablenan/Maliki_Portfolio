d7c14545d4033899b8f18829016c53aedfd2d912
tchablenan <kdjantchiemo@gmail.com>
2025-07-10 10:33:19 +0000

    comit

--- mon-portfolio/src/App.jsx
+++ mon-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './Pages/Home'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import './App.css'
